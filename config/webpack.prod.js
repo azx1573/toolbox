ee99fea71f60241ce4fde38f303f04abba6df9d5
azx1573 <anzixuan168@163.com>
2024-10-05 21:15:12 +0800

    refactor: optimize webpack loader use oneof

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -14,7 +14,7 @@ module.exports = {
     index: path.resolve(__dirname, "/src/index.js"),
   },
   mode: "production",
-  devtool: "inline-source-map",
+  devtool: "source-map",
   output: {
     filename: "static/js/[chunkhash:10].bundle.js",
     path: path.resolve(__dirname, "../dist"),
@@ -24,49 +24,53 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: generateStyleLoader(),
-      },
-      {
-        test: /\.less$/,
-        use: generateStyleLoader("less-loader"),
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: generateStyleLoader("sass-loader"),
-      },
-      {
-        test: /\.styl$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
-        use: generateStyleLoader("stylus-loader"),
-      },
-      // 使用webpack内置的资源模块类型处理图片文件(asset为通用资源模块，会自动根据文件大小选择base64或者file)
-      {
-        test: /\.(png|jpe?g|gif|webp|svg)$/,
-        type: "asset", // 通用资源模块类型，会自动根据文件大小选择base64或者file,默认8kb,可通过parser.dataUrlCondition.maxSize修改
-        parser: {
-          dataUrlCondition: {
-            maxSize: 8 * 1024,
+        oneOf: [
+          {
+            test: /\.ts$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
           },
-        },
-        generator: {
-          // 配置输出文件名，ext代表保留原文件扩展名
-          filename: "static/img/[hash:10][ext]",
-        },
-      },
-      // 使用webpack内置的资源模块类型处理字体文件(asset/resource为单独文件，会发起http请求获取)
-      {
-        test: /\.(eot|ttf|woff2?)$/,
-        type: "asset/resource", // 生成单独文件
-        generator: {
-          // 配置输出文件名，ext代表保留原文件扩展名
-          filename: "static/font/[hash:10][ext]",
-        },
+          {
+            test: /\.css$/,
+            use: generateStyleLoader(),
+          },
+          {
+            test: /\.less$/,
+            use: generateStyleLoader("less-loader"),
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: generateStyleLoader("sass-loader"),
+          },
+          {
+            test: /\.styl$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+            use: generateStyleLoader("stylus-loader"),
+          },
+          // 使用webpack内置的资源模块类型处理图片文件(asset为通用资源模块，会自动根据文件大小选择base64或者file)
+          {
+            test: /\.(png|jpe?g|gif|webp|svg)$/,
+            type: "asset", // 通用资源模块类型，会自动根据文件大小选择base64或者file,默认8kb,可通过parser.dataUrlCondition.maxSize修改
+            parser: {
+              dataUrlCondition: {
+                maxSize: 8 * 1024,
+              },
+            },
+            generator: {
+              // 配置输出文件名，ext代表保留原文件扩展名
+              filename: "static/img/[hash:10][ext]",
+            },
+          },
+          // 使用webpack内置的资源模块类型处理字体文件(asset/resource为单独文件，会发起http请求获取)
+          {
+            test: /\.(eot|ttf|woff2?)$/,
+            type: "asset/resource", // 生成单独文件
+            generator: {
+              // 配置输出文件名，ext代表保留原文件扩展名
+              filename: "static/font/[hash:10][ext]",
+            },
+          },
+        ],
       },
     ],
   },
