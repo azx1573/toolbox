df8f515cf76ab3d2a6c460a4425b759367028db9
azx1573 <anzixuan168@163.com>
2024-11-18 10:27:17 +0800

    docs:update Readme.md and others

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as arrayRemoveDuplicates} from './toolbox/arrayRemoveDuplicates/index';
 export { default as curryCase} from './toolbox/curryCase';
 export { default as debounce} from './toolbox/debounce';
-export { default as downloadWithTagA} from './toolbox/downloadWithTagA';
+export { default as downloadFile} from './toolbox/downloadFile';
 export { default as isEqual} from './toolbox/isEqual';
 export { default as promise} from './toolbox/promise/index';
 export { default as publisherAndSubscriber} from './toolbox/publisherAndSubscriber';
